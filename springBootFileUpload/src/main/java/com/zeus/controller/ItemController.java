b26089943face1c421bdce6cee2bb66be6e4d38e
rlaeogus0911 <rlaeogus0911@nate.com>
2026-02-02 16:14:28 +0900

    [수정] 이미지파일업로드에 업데이트 진행할때 파일선택하지 않고 기존의 파일명을 다시 사용하고자 할때 문제점발생됨을 확인했고, 해결 및 테스팅 완료

--- springBootFileUpload/src/main/java/com/zeus/controller/ItemController.java
+++ springBootFileUpload/src/main/java/com/zeus/controller/ItemController.java
@@ -138,29 +138,31 @@ public class ItemController {
 	public String itemUpdate(Item item, Model model) throws Exception {
 		log.info("/update item= " + item.toString());
 		MultipartFile file = item.getPicture();
-		String oldUrl = null; 
+		Item oldItem = itemService.read(item);
 
 		if (file != null && file.getSize() > 0) {
-			// 기존의 있는 외부저장소에 있는 파일을 삭제
-			Item oldItem = itemService.read(item);
-			oldUrl = oldItem.getUrl();
-
 			//새로운업로드 이미지파일
 			log.info("originalName: " + file.getOriginalFilename());
 			log.info("size: " + file.getSize());
 			log.info("contentType: " + file.getContentType());
 			String createdFileName = uploadFile(file.getOriginalFilename(), file.getBytes());
 			item.setUrl(createdFileName);
-
+			int count = itemService.update(item);
+			if (count > 0) {
+				//테이블에 수정내용이 완료가 되고 그리고 나서 이전 이미지 파일을 삭제한다.
+				if(oldItem.getUrl() != null) deleteFile(oldItem.getUrl());
+				model.addAttribute("message", "%s 상품수정 성공".formatted(item.getName()));
+				return "item/success";
+			}
+		}else {
+			item.setUrl(oldItem.getUrl());
+			int count = itemService.update(item);
+			if (count > 0) {
+				model.addAttribute("message", "%s 상품수정 성공".formatted(item.getName()));
+				return "item/success";
+			}
 		}
-		int count = itemService.update(item);
 		
-		if (count > 0) {
-			//테이블에 수정내용이 완료가 되고 그리고 나서 이전 이미지 파일을 삭제한다.
-			if(oldUrl != null) deleteFile(oldUrl);
-			model.addAttribute("message", "%s 상품수정 성공".formatted(item.getName()));
-			return "item/success";
-		}
 		model.addAttribute("message", "%s 상품수정 실패".formatted(item.getName()));
 		return "item/failed";
 	}
